870e1b3e83ad7858c444a3013f0ccfc3a2b5dc3b
Yeohuiju <157028681+Yeohuiju@users.noreply.github.com>
2024-07-09 19:32:01 +0900

    [Platinum V] Title: 버블 소트, Time: 116 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+static vector<int> A;
+static vector<int> tmp;
+static long count;
+
+void mergeSort(int s, int e);
+void merge(int s1, int s2, int e);
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+
+	cin >> N;
+	A = vector<int>(N, 0);
+	tmp = vector<int>(N, 0);
+
+	for (int i = 0; i < N; i++)
+		cin >> A[i];
+
+	count = 0;
+	mergeSort(0, N - 1);
+
+	cout << count;
+
+	return 0;
+}
+
+void mergeSort(int s, int e) {
+	if (s >= e)
+		return;
+
+	int m = (s + e) / 2;
+
+	mergeSort(s, m);
+	mergeSort(m + 1, e);
+	merge(s, m + 1, e);
+}
+
+void merge(int s1, int s2, int e) {
+	int i = s1, j = s2, idx = s1;
+
+	for (int k = s1; k <= e; k++)
+		tmp[k] = A[k];
+
+	while (i < s2 && j <= e) {
+		if (tmp[i] <= tmp[j])
+			A[idx++] = tmp[i++];
+		else {
+			A[idx++] = tmp[j++];
+			count += j - idx;
+		}
+	}
+
+	if (i == s2) {
+		for (int k = j; k <= e; k++)
+			A[idx++] = tmp[k];
+	}
+	else
+		for (int k = i; k < s2; k++)
+			A[idx++] = tmp[k];
+}
